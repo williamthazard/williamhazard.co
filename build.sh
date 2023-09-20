2154b219f79c10ac5093d16f8b8bcb7528d098cc
William Hazard <105560469+williamthazard@users.noreply.github.com>
2023-09-19 20:21:04 -0400

    make build.sh less copy pasty

--- build.sh
+++ build.sh
@@ -1,98 +1,35 @@
 echo ">> root .md to .html"
-
 list=$(ls -r ./*.md)
-for file in $list ; do
-  date=$(date -r ${file} +%D)
-  file=${file:2}
-  file=${file%.*}
-  echo "$file"
-  target=${file}.html
-  cat head.htm_ > ${target}
-  cmark --unsafe ${file}.md >> ${target}
-  cat foot.htm_ >> ${target}
-  sed -i '' -e 's#DATE#'$date'#g' ${target}
-done
+function htmlify() {
+  fold=$(basename $(pwd))
+  for file in $list ; do
+    date=$(date -r ${file} +%D)
+    file=${file:2}
+    file=${file%.*}
+    echo "$fold $file built"
+    target=${file}.html
+    cat $1 > ${target}
+    cmark --unsafe ${file}.md >> ${target}
+    cat $2 >> ${target}
+    sed -i '' -e 's#DATE#'$date'#g' ${target}
+  done
+}
+htmlify "head.htm_" "foot.htm_"
 cd words
-echo "entering words folder"
-for file in $list ; do
-  date=$(date -r ${file} +%D)
-  file=${file:2}
-  file=${file%.*}
-  echo "$file"
-  target=${file}.html
-  cat ../head.htm_ > ${target}
-  cmark --unsafe ${file}.md >> ${target}
-  cat ../foot.htm_ >> ${target}
-  sed -i '' -e 's#DATE#'$date'#g' ${target}
-done
+htmlify "../head.htm_" "../foot.htm_"
 cd ..
 cd sounds
-echo "entering sounds folder"
-for file in $list ; do
-  date=$(date -r ${file} +%D)
-  file=${file:2}
-  file=${file%.*}
-  echo "$file"
-  target=${file}.html
-  cat ../head.htm_ > ${target}
-  cmark --unsafe ${file}.md >> ${target}
-  cat ../foot.htm_ >> ${target}
-  sed -i '' -e 's#DATE#'$date'#g' ${target}
-done
+htmlify "../head.htm_" "../foot.htm_"
 cd ..
 cd videos
-echo "entering videos folder"
-for file in $list ; do
-  date=$(date -r ${file} +%D)
-  file=${file:2}
-  file=${file%.*}
-  echo "$file"
-  target=${file}.html
-  cat ../head.htm_ > ${target}
-  cmark --unsafe ${file}.md >> ${target}
-  cat ../foot.htm_ >> ${target}
-  sed -i '' -e 's#DATE#'$date'#g' ${target}
-done
+htmlify "../head.htm_" "../foot.htm_"
 cd ..
 cd code
-echo "entering code folder"
-for file in $list ; do
-  date=$(date -r ${file} +%D)
-  file=${file:2}
-  file=${file%.*}
-  echo "$file"
-  target=${file}.html
-  cat ../head.htm_ > ${target}
-  cmark --unsafe ${file}.md >> ${target}
-  cat ../foot.htm_ >> ${target}
-  sed -i '' -e 's#DATE#'$date'#g' ${target}
-done
+htmlify "../head.htm_" "../foot.htm_"
 cd ..
 cd performances
-echo "entering performances folder"
-for file in $list ; do
-  date=$(date -r ${file} +%D)
-  file=${file:2}
-  file=${file%.*}
-  echo "$file"
-  target=${file}.html
-  cat ../head.htm_ > ${target}
-  cmark --unsafe ${file}.md >> ${target}
-  cat ../foot.htm_ >> ${target}
-  sed -i '' -e 's#DATE#'$date'#g' ${target}
-done
+htmlify "../head.htm_" "../foot.htm_"
 cd ..
 cd log
-echo "entering log folder"
-for file in $list ; do
-  date=$(date -r ${file} +%D)
-  file=${file:2}
-  file=${file%.*}
-  echo "$file"
-  target=${file}.html
-  cat ../head.htm_ > ${target}
-  cmark --unsafe ${file}.md >> ${target}
-  cat ../foot.htm_ >> ${target}
-  sed -i '' -e 's#DATE#'$date'#g' ${target}
-done
+htmlify "../head.htm_" "../foot.htm_"
 cd ..
