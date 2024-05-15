b63ffd233e88abbdc03bc3aef988009ff0386386
William Hazard <105560469+williamthazard@users.noreply.github.com>
2024-05-15 05:00:15 -0400

    update

--- update.sh
+++ update.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 echo ">> pushing update"
 
+com='update'
+date=$(date)
+comdate=($com $date)
+
 git add .
-git commit -m 'update'
+git commit -m $comdate
 git push -u origin
 
 echo ">> update completed"
