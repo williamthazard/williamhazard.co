5146eafb5798b8879ff0b831e83bc4c1d90cf851
William Hazard <105560469+williamthazard@users.noreply.github.com>
2024-05-31 04:17:50 -0400

    update

--- sketches/liquid-text/sketch.js
+++ sketches/liquid-text/sketch.js
@@ -1,52 +1,76 @@
 let img, snd;
-let words = [
-  "i can yell that",
-  "he was wagering, but",
-  "it was justice for",
-  "he was weaseling",
-  "i can't tell what",
-  "he was waiting for",
-  "it was justice, but",
-  "he was wagering"
+let changeRate = 1;
+let choose = [];
+let lines = [
+  {
+    text: ["i can't yell that", "i can't tell why"],
+    currentText: 0,
+    bounce:[true, false], 
+    alpha:[0, 100]
+  },
+  {
+    text: ["he was waiting for", "he was winning but"],
+    currentText: 0,
+    bounce:[true, false],
+    alpha:[0, 100]
+  },
+  {
+    text: ["it was justice and", "it was phallic how"],
+    currentText: 0,
+    bounce:[true, false],
+    alpha:[0, 100]
+  },
+  {
+    text: ["he was weaseled", "he was wagering"],
+    currentText: 0,
+    bounce:[true, false],
+    alpha:[0, 100]
+  }
 ];
-let lines = [];
-let alphas = [100,100,100,100,0,0,0,0];
-let bounce = [];
-let chooser = [];
 
 function preload() {
   img = loadImage('assets/images/night-smaller.png');
   snd = loadSound('assets/sounds/voices-small.wav');
+  font = loadFont('assets/fonts/RobotoMono-VariableFont_wght.ttf');
 }
 
 function setup() {
   let cnv = createCanvas(windowWidth, windowHeight);
   cnv.mousePressed(canvasPressed);
   snd.playMode('restart');
+  frameRate(24);
+  textFont(font);
   if (deviceOrientation === LANDSCAPE) {
     img.resize(300,300);
     textSize(20);
-    for (let i = 0; i < 8; i += 1) {
-      lines[i] = text(words[i],(width/2)-200,(height/2)+(65+(40*i%4)));
-      bounce[i] = false;
-    }
+    lines.forEach(
+      (i,ind) => {
+        text(
+          lines[ind].text[lines[ind].currentText],
+          (width/2)-200,
+          (height/2)+(65+(40*ind))
+        );
+      }
+    );
   } else {
     img.resize(600,600);
     textSize(40);
-    for (let i = 0; i < 8; i += 1) {
-      lines[i] = text(words[i],(width/2)-400,(height/2)+(130+(60*i%4)));
-      bounce[i] = false;
-    }
+    lines.forEach(
+      (i,ind) => {
+        text(
+          lines[ind].text[lines[ind].currentText],
+          (width/2)-400,
+          (height/2)+(130+(60*ind))
+        );
+      }
+    )
   }
-  textFont('Courier New');
-  frameRate(24);
 }
 
 function canvasPressed() {
   if (snd.isLooping() === true) {
     snd.stop();
   } else if (snd.isLooping() === false) {
-    // start the loop
     snd.loop(0,1/2);
   }
 }
@@ -58,40 +82,79 @@ function draw() {
   } else {
     image(img, (width/2)-300, (height/2)-300);
   }
-  for (let i = 0; i < 4; i += 1) {
-    chooser[i] = random([0,1]);
-    if (chooser[i] === 0) {
-      chooser[i+4] = 1;
-    } else {
-      chooser[i+4] = 0;
+  
+  lines.forEach(
+    (i,ind) => {
+      choose[ind] = random([0,changeRate]);
+      if (deviceOrientation === LANDSCAPE) {
+        lines[ind].text.forEach(
+          (j,txt) => {
+            if(lines[ind].bounce[txt] === false) {
+              lines[ind].alpha[txt] = lines[ind].alpha[txt] - choose[ind];
+            } else if (lines[ind].bounce[txt] === true) {
+              lines[ind].alpha[txt] = lines[ind].alpha[txt] + choose[ind];
+            }
+            if(lines[ind].alpha[txt] <= 0) {
+              lines[ind].bounce[txt] = true;
+              lines[ind].alpha[txt] = 0;
+            } else if (lines[ind].alpha[txt] >= 100) {
+              lines[ind].bounce[txt] = false;
+              lines[ind].alpha[txt] = 100;
+            }
+            fill(255,255,255,lines[ind].alpha[txt]);
+            text(
+              lines[ind].text[txt],
+              (width/2)-110,
+              (height/2)-50+(40*(ind))
+            );
+          }
+        )
+      } else {
+        lines[ind].text.forEach(
+          (j,txt) => {
+            if(lines[ind].bounce[txt] === false) {
+              lines[ind].alpha[txt] = lines[ind].alpha[txt] - choose[ind];
+            } else if (lines[ind].bounce[txt] === true) {
+              lines[ind].alpha[txt] = lines[ind].alpha[txt] + choose[ind];
+            }
+            if(lines[ind].alpha[txt] <= 0) {
+              lines[ind].bounce[txt] = true;
+              lines[ind].alpha[txt] = 0;
+            } else if (lines[ind].alpha[txt] >= 100) {
+              lines[ind].bounce[txt] = false;
+              lines[ind].alpha[txt] = 100;
+            }
+            fill(255,255,255,lines[ind].alpha[txt]);
+            text(
+              lines[ind].text[txt],
+              (width/2)-225,
+              (height/2)-100+(80*(ind))
+            );
+          }
+        )
+      }
     }
-  }
-  for (let i = 0; i < 8; i += 1) {
-    if (alphas[i] === 0) {
-      bounce[i] = true;
-    } else if (alphas[i] === 100) {
-      bounce[i] = false;
-    }
-    fill(255,255,255,alphas[i]);
-    if (deviceOrientation === LANDSCAPE) {
-      lines[i] = text(words[i],(width/2)-110,(height/2)-50+(40*(i%4)));
-    } else {
-      lines[i] = text(words[i],(width/2)-225,(height/2)-100+(80*(i%4)));
-    }
-    if (bounce[i] === false && chooser[i] === 0) {
-      alphas[i] = alphas[i] - 1;
-    } else if (bounce[i] === true && chooser[i] === 1) {
-        alphas[i] = alphas[i] + 1;
-    }
-  }
+  )
   fill(255,255,255,75);
   if (snd.isLooping() === true && deviceOrientation === LANDSCAPE) {
     text("audio on",(width/2)+40,(height/2)+145);
   } else if (snd.isLooping() === false && deviceOrientation === LANDSCAPE) {
-    text("audio off",(width/2)+40,(height/2)+145)
+    text(
+      "audio off",
+      (width/2)+40,
+      (height/2)+145
+    )
   } else if (snd.isLooping() === true && deviceOrientation === PORTRAIT) {
-    text("audio on",(width/2)+80,(height/2)+290);
+    text(
+      "audio on",
+      (width/2)+80,
+      (height/2)+290
+    );
   } else if (snd.isLooping() === false && deviceOrientation === PORTRAIT) {
-    text("audio off",(width/2)+80,(height/2)+290);
+    text(
+      "audio off",
+      (width/2)+80,
+      (height/2)+290
+    );
   }
 }
