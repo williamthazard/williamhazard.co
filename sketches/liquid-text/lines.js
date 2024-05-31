3f30a142f19114121e7dca83e9a27d1f3752f434
William Hazard <105560469+williamthazard@users.noreply.github.com>
2024-05-31 04:49:29 -0400

    update

---
+++ sketches/liquid-text/lines.js
@@ -0,0 +1,26 @@
+let lines = [
+    {
+      text: ["i can't yell that", "i can't tell why"],
+      currentText: 0,
+      bounce:[true, false], 
+      alpha:[0, 100]
+    },
+    {
+      text: ["he was waiting for", "he was winning but"],
+      currentText: 0,
+      bounce:[true, false],
+      alpha:[0, 100]
+    },
+    {
+      text: ["it was justice and", "it was phallic how"],
+      currentText: 0,
+      bounce:[true, false],
+      alpha:[0, 100]
+    },
+    {
+      text: ["he was weaseled", "he was wagering"],
+      currentText: 0,
+      bounce:[true, false],
+      alpha:[0, 100]
+    }
+  ];
--- sketches/liquid-text/sketch.js
+++ sketches/liquid-text/sketch.js
@@ -1,32 +1,6 @@
 let img, snd;
 let changeRate = 1;
 let choose = [];
-let lines = [
-  {
-    text: ["i can't yell that", "i can't tell why"],
-    currentText: 0,
-    bounce:[true, false], 
-    alpha:[0, 100]
-  },
-  {
-    text: ["he was waiting for", "he was winning but"],
-    currentText: 0,
-    bounce:[true, false],
-    alpha:[0, 100]
-  },
-  {
-    text: ["it was justice and", "it was phallic how"],
-    currentText: 0,
-    bounce:[true, false],
-    alpha:[0, 100]
-  },
-  {
-    text: ["he was weaseled", "he was wagering"],
-    currentText: 0,
-    bounce:[true, false],
-    alpha:[0, 100]
-  }
-];
 
 function preload() {
   img = loadImage('assets/images/night-smaller.png');
