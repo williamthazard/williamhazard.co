7f738abf4d6e79a1945afecf0b31acba6da624bc
William Hazard <105560469+williamthazard@users.noreply.github.com>
2024-07-17 23:54:05 -0600

    update

--- build.sh
+++ build.sh
@@ -28,14 +28,30 @@ function addformer() {
 function addfirst() {
   if [[ $n > 10 ]]; then
       echo "<br/><br/><a href=../log/log0.html>[first]</a>" >> ../${log}.html
-    fi
+  fi
 }
 function addfinal() {
   if [[ $((pagenum-1)) == 0 ]]; then
       echo "<br/><br/><a href=../log/log${pages}.html>[final]</a>" >> ../${log}.html
     else
       echo "<a href=../log/log${pages}.html>[final]</a>" >> ../${log}.html
-    fi
+  fi
+}
+function paginate() {
+  if [[ $((n % 10)) == 0 ]]; then
+    echo "<p class='center'>" >> ../${log}.html
+    addformer
+    ((pagenum=$pagenum+1))
+    echo "<a href=../log/log${pagenum}.html>[further]</a>" >> ../${log}.html
+    addfirst
+    addfinal
+    echo "</p>" >> ../${log}.html
+    cat ../../log-foot.htm_ >> ../${log}.html
+    log="log"$pagenum
+    cat ../../head.htm_ > ../${log}.html
+    echo "<h1><p class='center'>log</p></h1>" >> ../${log}.html
+    echo "<p class='center'><a href=../index.html>[return]</a></p><br/>" >> ../${log}.html
+  fi
 }
 resize
 htmlify "head.htm_" "foot.htm_"
@@ -70,7 +86,6 @@ while [[ min -lt max ]] ; do
     x="${marks[$min]}"
     marks[$min]="${marks[$max]}"
     marks[$max]="$x"
-
     # Move closer
     (( min++, max-- ))
 done
@@ -87,21 +102,7 @@ for file in $marks ; do
   cmark --unsafe ${file}.md >> ${target}
   cat ../../log-foot.htm_ >> ${target}
   echo $name
-  # paginate
-  if [[ $((n % 10)) == 0 ]]; then
-    echo "<p class='center'>" >> ../${log}.html
-    addformer
-    ((pagenum=$pagenum+1))
-    echo "<a href=../log/log${pagenum}.html>[further]</a>" >> ../${log}.html
-    addfirst
-    addfinal
-    echo "</p>" >> ../${log}.html
-    cat ../../log-foot.htm_ >> ../${log}.html
-    log="log"$pagenum
-    cat ../../head.htm_ > ../${log}.html
-    echo "<h1><p class='center'>log</p></h1>" >> ../${log}.html
-    echo "<p class='center'><a href=../index.html>[return]</a></p><br/>" >> ../${log}.html
-  fi
+  paginate
   ((n=n+1))
   # append to index
   echo "<p><a href=entries/${target}>${name}</a></p>" >> ../${log}.html
