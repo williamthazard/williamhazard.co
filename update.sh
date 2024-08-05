d9aa67672c6a5f631a142ece44d0e86191d8b9c1
William Hazard <105560469+williamthazard@users.noreply.github.com>
2024-08-04 20:50:53 -0400

    update

--- update.sh
+++ update.sh
@@ -3,10 +3,12 @@ cd scripts
 zsh ./build.sh
 cd ..
 
-echo ">> pushing update"
+echo ">> clearing worktree"
+rm -f .git/index.lock
+echo ">> worktree cleared"
 
+echo ">> pushing update"
 git add .
 git commit -m 'update'
 git push -u origin
-
 echo ">> update complete"
