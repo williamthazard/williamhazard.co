b68ff5b2aa90012f7689245072bb32360d7c8493
williamthazard <williamthazard@pm.me>
2025-01-05 17:09:31 -0500

    update

--- sketches/curse-krypton/sketch.js
+++ sketches/curse-krypton/sketch.js
@@ -2,13 +2,13 @@ let ind = 0;
 function preload() {snd = loadSound('assets/bald-crow.mp3')}
 function setup() {let cnv = createCanvas(windowWidth, windowHeight);
   cnv.mousePressed(canvasPressed); snd.playMode('restart')}
-function draw() {background(35); textSize(50); fill('white'); frameRate(0.25);
+function draw() {
+  background(35); textSize(50); 
+  fill('white'); frameRate(0.25);
   ind = (frameCount*4)%lines.length;
   for (let x = 0; x < 5; x+= 1) {
     text(lines[(ind+x)%lines.length].text,
       lines[(ind+x)%lines.length].align[0],
-      lines[(ind+x)%lines.length].align[1])
-  }
-}
+      lines[(ind+x)%lines.length].align[1])}}
 function canvasPressed() {if (snd.isPlaying() === true) {snd.stop()} else {snd.play()}}
 function windowResized() {resizeCanvas(windowWidth, windowHeight)}
