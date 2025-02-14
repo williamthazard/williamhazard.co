7210e061f1ecfbe2277d035780e0b9cbedb2573e
williamthazard <williamthazard@pm.me>
2025-02-14 00:43:16 -0500

    update

--- scripts/izzzzi-post.py
+++ scripts/izzzzi-post.py
@@ -5,9 +5,8 @@ import sys
 
 num_args=len(sys.argv)-1
 post_arg = []
-
-for arg in range(num_args):
-    post_arg.append(sys.argv[arg+1])
+field = []
+types = ['text','img','file']
 
 # Set up the WebDriver
 driver = webdriver.Chrome()
@@ -34,20 +33,14 @@ driver.get("https://izzzzi.net/tomorrow")
 action_fields = driver.find_elements(By.NAME, "action")
 action_fields[1].click()
 
-# Locate and fill in the text field
-if len(post_arg) > 0:
-    text_field = driver.find_element(By.NAME, "text")
-    text_field.send_keys(post_arg[0])
-
-# Locate and fill in the image field
-if len(post_arg) > 1:
-    img_field = driver.find_element(By.NAME, "img")
-    img_field.send_keys(os.path.abspath(post_arg[1]))
-
-# Locate and fill in the file field
-if len(post_arg) > 2:
-    file_field = driver.find_element(By.NAME, "file")
-    file_field.send_keys(os.path.abspath(post_arg[2]))
+# Locate and fill in data fields
+for arg in range(num_args):
+    post_arg.append(sys.argv[arg+1])
+    field.append(driver.find_element(By.NAME, types[arg]))
+    if arg == 0:
+        field[arg].send_keys(post_arg[arg])
+    else:
+        field[arg].send_keys(os.path.abspath(post_arg[arg]))
 
 # Submit the form
 save_field = driver.find_element(By.NAME, "action")
