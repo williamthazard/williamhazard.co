434aaa95e9b9198e4c1feae55196250e07e6e8b1
williamthazard <williamthazard@pm.me>
2025-03-26 17:20:34 -0400

    update

--- scripts/redate.sh
+++ scripts/redate.sh
@@ -69,6 +69,8 @@ for file in $marks ; do
   touch -d ${new_date} ${file}
   file=${file%.*}
   echo "${file} | date: ${new_date}"
-  dateCheck
+  if [ $1 == 'socials' ] ; then
+    dateCheck
+  fi
 done
 cd ../../scripts
