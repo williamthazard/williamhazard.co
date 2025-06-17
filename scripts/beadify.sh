7fe55236c514b6fdf7a02e46eeaf0aacef7609b4
williamthazard <williamthazard@pm.me>
2025-06-17 12:48:22 -0400

    update

--- scripts/beadify.sh
+++ scripts/beadify.sh
@@ -1,16 +1,17 @@
 cd ../gbg
 echo ">> the glass bead game"
 function htmlify() {
-    for file in *.md; do
-      file=${file%.*}
-      folder=$(basename $(pwd))
-      echo "building $folder $file"
-      target=${file}.html
-      cat $1 > ${target}
-      cmark --unsafe ${file}.md >> ${target}
-      cat $2 >> ${target}
-      echo "$folder $file built"
-    done
+  touch marker.md
+  for file in *.md; do
+    file=${file%.*}
+    folder=$(basename $(pwd))
+    echo "building $folder $file"
+    target=${file}.html
+    cat $1 > ${target}
+    cmark --unsafe ${file}.md >> ${target}
+    cat $2 >> ${target}
+    echo "$folder $file built"
+  done
 }
 htmlify "../sub-head.htm_" "../gbg-foot.htm_"
 for subdir in ./*/ ; do
