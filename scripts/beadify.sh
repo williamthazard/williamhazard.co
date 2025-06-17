a393f384d50dea0467a901a62a49ad13331ef265
williamthazard <williamthazard@pm.me>
2025-06-17 12:43:49 -0400

    update

--- scripts/beadify.sh
+++ scripts/beadify.sh
@@ -1,22 +1,16 @@
 cd ../gbg
 echo ">> the glass bead game"
 function htmlify() {
-  count=`ls -1 *.md 2>/dev/null | wc -l`
-  if [ $count != 0 ]; then
     for file in *.md; do
       file=${file%.*}
       folder=$(basename $(pwd))
       echo "building $folder $file"
-      target=index.html
+      target=${file}.html
       cat $1 > ${target}
-      cmark --unsafe index.md >> ${target}
+      cmark --unsafe ${file}.md >> ${target}
       cat $2 >> ${target}
       echo "$folder $file built"
     done
-  else
-      folder=$(basename $(pwd))
-      echo("no markdown files detected in $folder")
-  fi
 }
 htmlify "../sub-head.htm_" "../gbg-foot.htm_"
 for subdir in ./*/ ; do
